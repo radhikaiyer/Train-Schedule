5d1070aaaae474a6af60257396c2ed6e9876b2b7
Radhika <radhikaiyer@Radhikas-MacBook-Air.local>
2018-03-15 18:29:27 -0700

    Finalized Game

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,86 @@
-# Train-Schedule
+// Initializing Firebase
+
+var config = {
+    apiKey: "AIzaSyD9dzxp2r7x3DEXTr8C3nc1ioCDdbEGBms",
+    authDomain: "train-schedule-60b0f.firebaseapp.com",
+    databaseURL: "https://train-schedule-60b0f.firebaseio.com",
+    projectId: "train-schedule-60b0f",
+    storageBucket: "",
+    messagingSenderId: "137369710663"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var currentTime = moment();
+
+database.ref().on("child_added", function(childSnap) {
+
+    var name = childSnap.val().name;
+    var destination = childSnap.val().destination;
+    var firstTrain = childSnap.val().firstTrain;
+    var frequency = childSnap.val().frequency;
+    var min = childSnap.val().min;
+    var next = childSnap.val().next;
+
+    $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
+
+database.ref().on("value", function(snapshot) {  
+
+});
+
+//We need to grab the information 
+
+$("#addTrainBtn").on("click", function() {
+
+    var trainName = $("#NameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = $("#firstInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+
+    //This will ensure that each input has a value
+    if (trainName == "") {
+        alert('Enter a train name.');
+        return false;
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
+        return false;
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
+        return false;
+    }
+
+    // This whole section will be the math section 
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+
+    // the time difference between current time and the first train
+    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        name: Name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        min: minUntilTrain,
+        next: nextTrain
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#NameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstInput").val("");
+    $("#frequencyInput").val("");
+
+    return false;
+});
